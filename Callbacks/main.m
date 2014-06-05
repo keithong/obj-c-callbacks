c17b0c66e4cd776e55ff331225f6a98c6ff26838
Keith Samson <k.samson@klab.com>
2014-06-05 11:16:37 +0800

    Callbacks' initial commit

---
+++ Callbacks/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Callbacks
+//
+//  Created by Keith Samson on 6/5/14.
+//  Copyright (c) 2014 Keith Samson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Callbacks for Objective-C!");
+        
+    }
+    return 0;
+}
+
